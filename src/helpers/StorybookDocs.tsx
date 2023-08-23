4c3d09128d8caa1a32137cb4390d8f784ae12b6e
APJohns <mail@ashpjohns.com>
2023-08-23 13:41:03 -0400

    add todos

--- src/helpers/StorybookDocs.tsx
+++ src/helpers/StorybookDocs.tsx
@@ -1,6 +1,8 @@
 import { Title, Primary, Controls, Subtitle, Description } from '@storybook/blocks';
 
 const StorybookDocs = () => {
+  // TODO: Review which blocks we want to use and in what order
+  // TODO: Use this to define autodocs
   return (
     <>
       <Title />
