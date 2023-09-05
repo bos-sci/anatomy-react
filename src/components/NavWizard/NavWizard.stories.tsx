d52f78ac8f4eb6f6200b082ab753720c7a60ca69
Kasey Labelle <labellk@Kaseys-MacBook-Pro.local>
2023-09-05 16:18:37 -0400

    add wizard nav story

---
+++ src/components/NavWizard/NavWizard.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import NavWizard from './NavWizard';
+import navWizardData from './navWizardData-3Step';
+
+const meta = {
+  title: 'Components/NavWizard',
+  component: NavWizard,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+} satisfies Meta<typeof NavWizard>;
+
+export default meta;
+type Story = StoryObj<typeof NavWizard>;
+
+export const Playground: Story = {
+  render: (args) => (
+    <NavWizard
+      {...args}
+      texts={{
+        firstTitle: 'First step title',
+        wizardNavAriaLabel: 'wizard'
+      }}
+      navItems={navWizardData}
+    />
+  )
+};
