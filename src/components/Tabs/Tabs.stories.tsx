1a111194aec20af803ffde0b6ee7eb67b030378a
APJohns <mail@ashpjohns.com>
2023-09-06 12:31:55 -0400

    update tab stories

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -13,26 +13,12 @@ type Story = StoryObj<typeof Tabs>;
 export const Playground: Story = {
   render: (args) => (
     <Tabs tablistLabel="Demo tabs">
-      <TabPanel tabName="Tab 1">Tab panel 1</TabPanel>
-      <TabPanel tabName="Tab 2">Tab panel 2</TabPanel>
-      <TabPanel tabName="Tab 3">Tab panel 3</TabPanel>
-    </Tabs>
-  )
-};
-
-export const Overflow: Story = {
-  render: (args) => (
-    <Tabs tablistLabel="Demo tabs">
-      <TabPanel tabName="Tab 1">Tab panel 1</TabPanel>
+      <TabPanel tabName="Tab 1">Resize the browser to see how tabs overflow</TabPanel>
       <TabPanel tabName="Tab 2">Tab panel 2</TabPanel>
       <TabPanel tabName="Tab 3">Tab panel 3</TabPanel>
       <TabPanel tabName="Tab 4">Tab panel 4</TabPanel>
       <TabPanel tabName="Tab 5">Tab panel 5</TabPanel>
       <TabPanel tabName="Tab 6">Tab panel 6</TabPanel>
-      <TabPanel tabName="Tab 7">Tab panel 7</TabPanel>
-      <TabPanel tabName="Tab 8">Tab panel 8</TabPanel>
-      <TabPanel tabName="Tab 9">Tab panel 9</TabPanel>
-      <TabPanel tabName="Tab 10">Tab panel 10</TabPanel>
     </Tabs>
   )
 };
