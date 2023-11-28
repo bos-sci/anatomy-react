809832d40400f8f482614e84b7fa8c4a5c6455ef
cblva <chrisbuylova@gmail.com>
2023-11-28 16:10:30 -0500

    Fix logic in NavPrimary

--- src/components/NavPrimary/NavPrimary.tsx
+++ src/components/NavPrimary/NavPrimary.tsx
@@ -302,16 +302,13 @@ const NavPrimary = ({
       )}
       <nav className="bsds-nav-primary" aria-label={texts?.primaryNavAriaLabel || 'primary'}>
         <div className="bsds-nav-bar">
-          {
-            // eslint-disable-next-line react/jsx-no-leaked-render
-            (logo.to || logo.href) && !logoSecondary ? (
-              <Link to={logo.to} href={logo.href} className="bsds-link-logo" isNavLink>
-                <img src={logo.src} alt={logo.alt} className="bsds-nav-link-logo" />
-              </Link>
-            ) : (
-              <img className="bsds-nav-link-logo" src={logo.src} alt={logo.alt} />
-            )
-          }
+          {logo.to || logo.href ? (
+            <Link to={logo.to} href={logo.href} className="bsds-link-logo" isNavLink>
+              <img src={logo.src} alt={logo.alt} className="bsds-nav-link-logo" />
+            </Link>
+          ) : (
+            <img className="bsds-nav-link-logo" src={logo.src} alt={logo.alt} />
+          )}
           <ul className="bsds-nav" role="menubar" onKeyUp={handleKeyUp}>
             {navTree.map((navItem, i) => (
               <li
