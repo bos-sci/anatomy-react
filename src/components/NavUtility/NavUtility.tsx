b7ada2ab5943b10024a95d6a81897b6ec155eacd
cblva <chrisbuylova@gmail.com>
2023-11-29 13:40:07 -0500

    Fix cobranded nav for constrained style

--- src/components/NavUtility/NavUtility.tsx
+++ src/components/NavUtility/NavUtility.tsx
@@ -14,28 +14,30 @@ export interface NavUtilityProps {
 const NavUtility = ({ utilityItems, ariaLabel = 'Utility', logoSecondary }: NavUtilityProps): JSX.Element => {
   return (
     <nav className="bsds-nav-utility" aria-label={ariaLabel}>
-      {!!logoSecondary?.src && (
-        <img className="bsds-nav-logo-secondary" src={logoSecondary.src} alt={logoSecondary.alt} />
-      )}
-      <ul className="bsds-nav">
-        {utilityItems.map((utilityItem) => (
-          <li key={'utilityItem' + utilityItem.text} className="bsds-nav-item">
-            {utilityItem.children ? (
-              <DropdownMenu triggerText={utilityItem.text} className="bsds-nav-link" menuPosition="bottom-end">
-                {utilityItem.children.map((child) => (
-                  <Link key={child.text + (child.to || child.href)} href={child.href} to={child.to}>
-                    {child.text}
-                  </Link>
-                ))}
-              </DropdownMenu>
-            ) : (
-              <Link to={utilityItem.to} href={utilityItem.href} className="bsds-nav-link">
-                {utilityItem.text}
-              </Link>
-            )}
-          </li>
-        ))}
-      </ul>
+      <div className="bsds-nav-utility-container">
+        {!!logoSecondary?.src && (
+          <img className="bsds-nav-logo-secondary" src={logoSecondary.src} alt={logoSecondary.alt} />
+        )}
+        <ul className="bsds-nav">
+          {utilityItems.map((utilityItem) => (
+            <li key={'utilityItem' + utilityItem.text} className="bsds-nav-item">
+              {utilityItem.children ? (
+                <DropdownMenu triggerText={utilityItem.text} className="bsds-nav-link" menuPosition="bottom-end">
+                  {utilityItem.children.map((child) => (
+                    <Link key={child.text + (child.to || child.href)} href={child.href} to={child.to}>
+                      {child.text}
+                    </Link>
+                  ))}
+                </DropdownMenu>
+              ) : (
+                <Link to={utilityItem.to} href={utilityItem.href} className="bsds-nav-link">
+                  {utilityItem.text}
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
