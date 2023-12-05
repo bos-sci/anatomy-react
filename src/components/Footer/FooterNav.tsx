ee785fd5af8de7bdbab3482640868651c0155708
Ashley Johns <mail@ashpjohns.com>
2023-12-05 14:50:15 -0500

    turn off unused lint ignores

--- src/components/Footer/FooterNav.tsx
+++ src/components/Footer/FooterNav.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
 import { useEffect, useId, useState } from 'react';
 import { NavItem, RequireOnlyOne } from '../../types';
 import Link from '../Link';
