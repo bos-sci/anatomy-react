01b42d919640d566f54db35cff53c33340bfdfa0
Ashley Johns <mail@ashpjohns.com>
2023-12-05 14:49:09 -0500

    clean up story and component

---
+++ src/components/Footer/footerData.ts
@@ -0,0 +1,119 @@
+import { NavItemsFooter } from './FooterNav';
+
+export const navItems: NavItemsFooter[] = [
+  {
+    title: 'Group 1',
+    navItems: [
+      {
+        text: 'Item 1',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 2',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 3',
+        href: 'docs-demo-link'
+      }
+    ]
+  },
+  {
+    title: 'Group 2',
+    navItems: [
+      {
+        text: 'Item 1',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 2',
+        href: 'docs-demo-link'
+      }
+    ]
+  },
+  {
+    title: 'Group 3',
+    navItems: [
+      {
+        text: 'Item 1',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 2',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 3',
+        href: 'docs-demo-link'
+      }
+    ]
+  },
+  {
+    title: 'Group 4',
+    navItems: [
+      {
+        text: 'Item 1',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 2',
+        href: 'docs-demo-link'
+      }
+    ]
+  },
+  {
+    title: 'Group 5',
+    navItems: [
+      {
+        text: 'Item 1',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 2',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 3',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 4',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 5',
+        href: 'docs-demo-link'
+      }
+    ]
+  },
+  {
+    title: 'Group 6',
+    navItems: [
+      {
+        text: 'Item 1',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 2',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 3',
+        href: 'docs-demo-link'
+      }
+    ]
+  },
+  {
+    title: 'Group 7',
+    navItems: [
+      {
+        text: 'Item 1',
+        href: 'docs-demo-link'
+      },
+      {
+        text: 'Item 2',
+        href: 'docs-demo-link'
+      }
+    ]
+  }
+];
