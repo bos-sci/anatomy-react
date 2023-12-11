057ae7fbd307c9c21db4bd260e2329bcd908d865
cblva <chrisbuylova@gmail.com>
2023-12-11 14:17:36 -0500

    Fix test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 describe('Footer', () => {
   it('Renders nav items when passed navItems', () => {
     render(<Footer navItems={navItems} />);
-    expect(screen.getAllByText(/Group [0-9]/)).toHaveLength(7);
-    expect(screen.getAllByText(/Item [0-9]/)).toHaveLength(20);
+    expect(screen.getAllByText(/Page group [0-9]/)).toHaveLength(7);
+    expect(screen.getAllByText(/Page [0-9]/)).toHaveLength(20);
   });
 });
