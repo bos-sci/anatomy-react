54fe8286064758ace7fe676c3d9c66b2a6eecad2
Ashley Johns <mail@ashpjohns.com>
2024-02-01 15:58:27 -0500

    add sidebar filter to hide stories with "hidden" tag

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -2,5 +2,10 @@ import { addons } from '@storybook/manager-api';
 import theme from './theme';
 
 addons.setConfig({
-  theme
+  theme,
+  sidebar: {
+    filters: {
+      hiddenStories: (item) => !item.tags?.includes('hidden')
+    }
+  }
 });
